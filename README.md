4b0273627240f5e1227a37232be9c73a960f5df7
Markus H <markus@mawkke.fi>
2024-05-02 15:41:52 +0300

    Update README.md

--- README.md
+++ README.md
@@ -19,6 +19,23 @@ Run the following to run the main experiment
 
     $ make
 
+Example output:
+
+    $ make
+    cargo build
+        Finished dev [unoptimized + debuginfo] target(s) in 0.02s
+    ---
+    racket main.rkt
+    >>> Racket begin <<<
+    Hello from Rust!
+    rust_add_ints(39, 3) = 42
+    rust_add_floats(41.32, 1.69)  = 43.0099983215332
+    rust_add_doubles(41.32, 1.69) = 43.01
+    sum((1.0 0.5 0.3333333333333333 0.25 0.2)) = 2.283333333333333
+    rust_sum_complex(#(1+2i 3+1/2i 2/5+5i)) = 4.4+7.5i
+    rust_sum_ints(#(0 1 2 3 4 5 6 7 8 9 10)) = 55
+    >>> Racket end <<<
+
 See `Makefile` for other details
 
 ## Author
--- main.rkt
+++ main.rkt
@@ -2,15 +2,17 @@
 
 (require (prefix-in rust: "bindings.rkt"))
 
+(displayln ">>> Racket begin <<<")
+
 (rust:hello)
 
 (let ([a 39] [b 3])
-    (printf "add_ints(~a, ~a) = ~a~%" a b (rust:add-ints a b)))
+    (printf "rust_add_ints(~a, ~a) = ~a~%" a b (rust:add-ints a b)))
 
 (let ([a 41.32] [b 1.69])
     ; note difference in precision
-    (printf "add_float(~a, ~a)  = ~a~%" a b (rust:add-floats a b))
-    (printf "add_double(~a, ~a) = ~a~%" a b (rust:add-doubles a b)))
+    (printf "rust_add_floats(~a, ~a)  = ~a~%" a b (rust:add-floats a b))
+    (printf "rust_add_doubles(~a, ~a) = ~a~%" a b (rust:add-doubles a b)))
 
 (define (recip x) (/ 1 x))
 
@@ -19,7 +21,9 @@
 
 (define c-data '#(1+2i 3+2/4i 4/10+5i))
 (define res (rust:sum-complex c-data))
-(printf "sum-complex(~a) = ~a~%" c-data res)
+(printf "rust_sum_complex(~a) = ~a~%" c-data res)
 
 (define num-list (list->vector (range 11)))
-(printf "sum-ints(~a) = ~a~%" num-list (rust:sum-ints num-list))
+(printf "rust_sum_ints(~a) = ~a~%" num-list (rust:sum-ints num-list))
+
+(displayln ">>> Racket end <<<")
